04ebd6a5afd158e4d4cbce6ca0ef95568ab03f8b
Yuri Furusho Becker <yurifbeck@gmail.com>
2017-05-22 12:06:20 -0400

    bacon

--- eztrut/eztrut.py
+++ eztrut/eztrut.py
@@ -59,6 +59,24 @@ class Apoio:
     def __init__(self, x, tipo=1):
         self.x = x
         self.tipo = tipo
+        self.reacao = 0
+        self.nome = ''
+
+    def __cmp__(self, other):
+        if hasattr(other, 'getKey'):
+            return self.getKey().__cmp__(other.getKey())
+
+    def getKey(self):
+        return self.x
+
+    def __repr__(self):
+        return '{} {}: x = {}, tipo = {}, reacao = {}'.format(
+            self.__class__.__name__,
+            self.nome,
+            self.x,
+            self.tipo,
+            self.reacao
+        )
 
 
 class ForcaC:
@@ -112,6 +130,12 @@ class Eztrut:
         self.apoios = apoios
         self.cargas = cargas
         self.fig = plt.figure()
+        # self.reacao_a = 0
+        # self.reacao_b = 0
+        self.dist = 0
+        self.soma_forcas_x = 0
+        self.soma_forcas_y = 0
+        self.soma_forcas_z = 0
 
         if self.estaticidade() > 3:
             print("! ------ERRO ------ ! \n" +
@@ -130,7 +154,7 @@ class Eztrut:
     def plotagem_apoios(self):
         """Plota apoios"""
         for apoio in self.apoios:
-            plt.plot([apoio.x], [0], marker='^', markersize=12, fillstyle='none')
+            plt.plot([apoio.x], [0], marker=6, markersize=15, fillstyle='full')
 
     def plotagem_forcasC(self):
         """Plota forcas concentradas"""
@@ -139,14 +163,38 @@ class Eztrut:
         for forca in self.cargas:
             if isinstance(forca, ForcaC):
                 plt.annotate("{} kN".format(abs(forca.f / 1000)), xy=(forca.x, 0), xytext=(
-                   0, 40), textcoords='offset pixels', arrowprops=dict(arrowstyle="-|>"),
+                    0, 40), textcoords='offset pixels', arrowprops=dict(arrowstyle="-|>"),
                              horizontalalignment='center')
 
     def plotagem_viga(self):
         """Plota viga"""
         plt.plot([0, self.viga.comprimento], [0, 0], linewidth=4, alpha=0.3)
         plt.xlabel('x [m]')
-        plt.ylabel('Momento fletor [kNm]')
+        # plt.ylabel('Momento fletor [kNm]')
+        plt.yticks([])
+
+    def plotagem_reacao_apoio(self):
+        """Plota as flechas de reacao de apoio """
+        for apoio in self.apoios:
+            if apoio.reacao != 0:
+
+                if apoio.reacao > 0:
+                    posicao_texto = 40
+                else:
+                    posicao_texto = -40
+
+                plt.annotate(
+                    "{} kN".format(abs(apoio.reacao/1000)),
+                    xy=(apoio.x, 0),
+                    xytext=(0, posicao_texto),
+                    textcoords='offset pixels',
+                    arrowprops=dict(
+                        arrowstyle='simple',
+                        facecolor='red'
+                    ),
+                    horizontalalignment='center'
+                )
+
 
     def mostrar_figura(self):
         """Plota viga, forcas e apoios"""
@@ -156,23 +204,40 @@ class Eztrut:
         self.plotagem_forcasC()
         plt.margins(0.2, 0)
         plt.show()
-    
+
+    def ordenar_apoios(self, apoios_array):
+        """Retorna uma lista ordenada em funcao de X (posicao) e da nome aos apoios"""
+
+        def getKey(elemento):
+            return elemento.x
+
+        array_ordenado = sorted(apoios_array, key=getKey)
+        letra = 'A'
+
+        for apoio in array_ordenado:
+            apoio.nome = letra
+            letra = chr(ord(letra) + 1)
+
+        return array_ordenado
+
+
+
     def cortante(self):
-        fx=0
-        fy=0
-        fz=0
+        fx = 0
+        fy = 0
+        fz = 0
 
         # Soma das forcas
         for carga in self.cargas:
             if carga.tipo == 1:
-                fx+=carga.f
+                fx += carga.f
             if carga.tipo == 2:
-                fy+=carga.f
+                fy += carga.f
             if carga.tipo == 3:
-                fz+=carga.f
+                fz += carga.f
         # vetor com a soma das forcas em todas direcoes
         f = [fx, fy, fz]
-        
+
 
     def reacao_apoio_biapoiada_isoestatica(self):
         # determinar cortante em viga bi apoiada isoestatica
@@ -180,15 +245,15 @@ class Eztrut:
         # momento causado pela forca no apoio A
         m = 0
 
-        # Soma de forcas em y
-        fy = 0
+        # ordenar em ordem crescente da esquuerda para a direita os apoios
+        self.apoios = self.ordenar_apoios(self.apoios)
 
         # posicao do apoio A
         x = self.apoios[0].x
 
         for carga in self.cargas:
             if carga.tipo == 2: # se a carga for do tipo vertical apenas
-                fy += carga.f
+                self.soma_forcas_y += carga.f
                 if carga.x < x: # se a forca estiver aa esquerda do apoio A
                     m += carga.f * (x - carga.x)
                 else:
@@ -197,12 +262,56 @@ class Eztrut:
         # A reacao do apoio B tem que ser m dividido pela distancia dos apoios
         # (M = Rb * dist) -> (Rb = M / dist)
         # Eh importante que os apoios estejam em ordem da esquerda para direita
-        dist = self.apoios[1].x - self.apoios[0].x
-        reacao_b = m / dist
-        reacao_a = fy - reacao_b
+        self.dist = self.apoios[1].x - self.apoios[0].x
+        self.apoios[1].reacao = m / self.dist
+        self.apoios[0].reacao = - self.soma_forcas_y - self.apoios[1].reacao
 
-        return "Soma de forcas em y: {} kN \n Apoio A: {} kN \n Apoio B: {} \n".format(
-            fy/1000, reacao_a/1000, reacao_b/1000)
+        return print(" Soma de forcas em y: {} kN \n Apoio A: {} kN \n Apoio B: {} kN \n".format(
+            self.soma_forcas_y/1000, self.apoios[0].reacao/1000, self.apoios[1].reacao/1000))
+
+
+    def memorial_reacao(self):
+        from IPython.display import display, Math, Latex, Markdown
+
+        def forca_x(self):
+            completo = False
+            for forca in self.forcas:
+                if forca.tipo == 1:
+                    completo  = True
+            
+            if completo == True:
+                frase = (r'\sum F_x = 0 \rightarrow R_a + R_b = ' +
+                    str(self.soma_forcas_x) + r'\\')
+            else:
+                frase = r'\sum F_x = 0 \\'
+
+            return frase
+
+        def forca_y(self):
+            completo = False
+            for forca in self.forcas:
+                if forca.tipo == 2:
+                    completo  = True
+            
+            if completo == True:
+                frase = (r'\sum F_y = 0 \rightarrow R_a + R_b = ' +
+                    str(self.soma_forcas_y) + r'\\')
+            else:
+                frase = r'\sum F_y = 0 \\'
+
+            return frase
+
+ 
+        sums = Math(
+            r'\sum F_x = 0 \\' +
+            forca_x() + 
+            r'\sum F_y = 0 \rightarrow R_a + R_b = ' +
+            str(self.soma_forcas_y) + r'\\' +
+            r'\sum M_a = 0 \rightarrow R_b \dot ' +
+            str(self.dist) + r'\\'
+        )
+        return sums
+
 
 
 
